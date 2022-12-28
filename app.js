1081ecae10d2d2610ad84fc3a0f22b8cad3bc1be
Leonardo Luz <leonardoluz10@hotmail.com>
2022-12-28 17:31:53 -0300

    Auth com node.js, express, jwt, bcrypt e mongoose.

--- app.js
+++ app.js
@@ -23,20 +23,36 @@ db.once('open', () => {
 app.get('/', async (req, res) => {
     res.status(200).json({ message: "Bem vindo a nossa API!" })
 })
-
 // Private Route
-app.get('/user/:id', async(req, res) => {
+app.get('/user/:id', checkToken, async(req, res) => {
     const id = req.params.id;
 
     // check if user exists
     const user = await User.findById(id,'-password');
     
     if(!user){
-        return res.status(404).json({msg: 'Usuario não encontrado!'})
+        res.status(404).json({msg: 'Usuario não encontrado!'})
     }
+    res.status(200).json({user})
 
 })
+function checkToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
 
+    if(!token){
+        return res.status(401).json({msg: 'Acesso negado!'})
+    }
+
+    try {
+        const secret = process.env.SECRET
+        jwt.verify(token, secret)
+        next()
+    } catch (error) {
+        res.status(400).json({msg: 'Token inválido!'})
+    }
+
+}
 // Register User
 app.post('/auth/register', async (req, res) => {
     const { name, email, password, confirmPassword } = req.body;
@@ -104,7 +120,6 @@ app.post('/auth/login', async (req, res) => {
     if (!checkPassword) {
         res.status(422).json({ msg: `Senha inválida!` })
     }
-
     try {
 
         const secret = process.env.SECRET;
@@ -123,7 +138,6 @@ app.post('/auth/login', async (req, res) => {
     }
 
 })
-
 app.listen(port, () => {
     console.log(`Servidor escutando na porta http://localhost:${port}`)
 })
